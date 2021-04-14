3d8a1f637a41334ce2162ce4ee6d7b9e83819c7b
lytico <lytico@users.sourceforge.net>
2021-04-14 05:26:25 +0200

    GraphicsTester.Gtk.csproj: enhance

--- samples/GraphicsTester.Gtk/MainWindow.cs
+++ samples/GraphicsTester.Gtk/MainWindow.cs
@@ -5,128 +5,112 @@ using Gtk;
 using System;
 using System.Collections.Generic;
 using System.IO;
+using GraphicsTester.Scenarios;
 using Microsoft.Maui.Graphics;
 using Microsoft.Maui.Graphics.Skia;
 
-namespace Samples
-{
-    class MainWindow : Window
-    {
+namespace Samples {
+
+    class MainWindow : Window {
+
         private HeaderBar _headerBar;
         private TreeView _treeView;
         private Box _boxContent;
         private TreeStore _store;
-        private Dictionary<string, (Type type, Widget widget)> _items;
+        private Dictionary<string, AbstractScenario> _items;
         private Notebook _notebook;
         private GtkSkiaGraphicsView _skiaGraphicsView;
         private GtkSkiaDirectRenderer _skiaGraphicsRenderer;
-        public MainWindow() : base(WindowType.Toplevel)
-        {
+
+        public MainWindow () : base (WindowType.Toplevel) {
             // Setup GUI
             WindowPosition = WindowPosition.Center;
-            DefaultSize = new Gdk.Size(800, 600);
+            DefaultSize = new Gdk.Size (800, 600);
 
-            _headerBar = new HeaderBar();
+            _headerBar = new HeaderBar ();
             _headerBar.ShowCloseButton = true;
             _headerBar.Title = "GtkSharp Sample Application";
 
-            var btnClickMe = new Button();
+            var btnClickMe = new Button ();
             btnClickMe.AlwaysShowImage = true;
-            btnClickMe.Image = Image.NewFromIconName("document-new-symbolic", IconSize.Button);
-            _headerBar.PackStart(btnClickMe);
+            btnClickMe.Image = Image.NewFromIconName ("document-new-symbolic", IconSize.Button);
+            _headerBar.PackStart (btnClickMe);
 
             Titlebar = _headerBar;
 
-            var hpanned = new HPaned();
+            var hpanned = new HPaned ();
             hpanned.Position = 200;
 
-            _treeView = new TreeView();
+            _treeView = new TreeView ();
             _treeView.HeadersVisible = false;
-            hpanned.Pack1(_treeView, false, true);
+            hpanned.Pack1 (_treeView, false, true);
 
-            _notebook = new Notebook();
+            Fonts.Register (new SkiaFontService ("", ""));
+            GraphicsPlatform.RegisterGlobalService (SkiaGraphicsService.Instance);
 
-            var scroll1 = new ScrolledWindow();
-            var vpanned = new VPaned();
-            vpanned.Position = 300;
-            _boxContent = new Box(Orientation.Vertical, 0);
-            _boxContent.Margin = 8;
-            vpanned.Pack1(_boxContent, true, true);
-
-            Fonts.Register(new SkiaFontService("",""));
-            GraphicsPlatform.RegisterGlobalService(SkiaGraphicsService.Instance);
-
-            _skiaGraphicsView = new GtkSkiaGraphicsView ();
+            _skiaGraphicsView = new GtkSkiaGraphicsView {
+                BackgroundColor = Colors.White
+            };
             _skiaGraphicsRenderer = new GtkSkiaDirectRenderer ();
             _skiaGraphicsView.Renderer = _skiaGraphicsRenderer;
 
-            vpanned.Pack2(_skiaGraphicsView, false, true);
-            scroll1.Child = vpanned;
-            _notebook.AppendPage(scroll1, new Label { Text = "Data", Expand = true });
+            var scroll1 = new ScrolledWindow ();
+            scroll1.Child = _skiaGraphicsView;
 
-            var scroll2 = new ScrolledWindow();
+            hpanned.Pack2 (scroll1, true, true);
 
-
-            _notebook.AppendPage(scroll2, new Label { Text = "Code", Expand = true });
-
-            hpanned.Pack2(_notebook, true, true);
 
             Child = hpanned;
 
             // Fill up data
-            FillUpTreeView();
+            FillUpTreeView ();
 
             // Connect events
             _treeView.Selection.Changed += Selection_Changed;
-            Destroyed += (sender, e) => Application.Quit();
+            Destroyed += (sender, e) => Application.Quit ();
+
+            var scenario = ScenarioList.Scenarios[0];
+            _skiaGraphicsView.Drawable = scenario;
         }
 
-        private void Selection_Changed(object sender, EventArgs e)
-        {
-            if (_treeView.Selection.GetSelected(out TreeIter iter))
-            {
-                var s = _store.GetValue(iter, 0).ToString();
+        private void Selection_Changed (object sender, EventArgs e) {
+            if (_treeView.Selection.GetSelected (out TreeIter iter)) {
+                var s = _store.GetValue (iter, 0).ToString ();
 
-                while (_boxContent.Children.Length > 0)
-                    _boxContent.Remove(_boxContent.Children[0]);
-                _notebook.CurrentPage = 0;
-                _notebook.ShowTabs = false;
-
-                if (_items.TryGetValue(s, out var item))
-                {
-                    _notebook.ShowTabs = true;
-
-                    if (item.widget == null)
-                        _items[s] = item = (item.type, Activator.CreateInstance(item.type) as Widget);
-
-
-                    _boxContent.PackStart(item.widget, true, true, 0);
-                    _boxContent.ShowAll();
+                if (_items.TryGetValue (s, out var scenario)) {
+                    _skiaGraphicsView.Drawable = scenario;
                 }
 
             }
         }
 
-        private void FillUpTreeView()
-        {
+        private void FillUpTreeView () {
             // Init cells
-            var cellName = new CellRendererText();
+            var cellName = new CellRendererText ();
 
             // Init columns
-            var columeSections = new TreeViewColumn();
+            var columeSections = new TreeViewColumn ();
             columeSections.Title = "Sections";
-            columeSections.PackStart(cellName, true);
+            columeSections.PackStart (cellName, true);
 
-            columeSections.AddAttribute(cellName, "text", 0);
+            columeSections.AddAttribute (cellName, "text", 0);
 
-            _treeView.AppendColumn(columeSections);
+            _treeView.AppendColumn (columeSections);
 
             // Init treeview
-            _store = new TreeStore(typeof(string));
+            _store = new TreeStore (typeof(string));
             _treeView.Model = _store;
+            _items = new Dictionary<string, AbstractScenario>();
+            foreach (var scenario in ScenarioList.Scenarios) {
 
+                _store.AppendValues (scenario.ToString ());
+                _items[scenario.ToString ()] = scenario;
 
-            _treeView.ExpandAll();
+            }
+
+            _treeView.ExpandAll ();
         }
+
     }
+
 }
