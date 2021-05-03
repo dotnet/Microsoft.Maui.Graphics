3e94380db3e5f45343851a947846915494fbb042
lytico <lytico@users.sourceforge.net>
2021-05-04 00:14:12 +0200

    Microsoft.Maui.Graphics.Gtk: introduce NativeGraphicsService - Stub

---
+++ src/Microsoft.Maui.Graphics.Gtk/Gtk/NativeGraphicsService.cs
@@ -0,0 +1,29 @@
+using System.IO;
+
+namespace Microsoft.Maui.Graphics.Native.Gtk {
+
+	public class NativeGraphicsService : IGraphicsService {
+
+		public string SystemFontName { get; set; }
+
+		public string BoldSystemFontName { get; set; }
+
+		public SizeF GetStringSize(string value, string fontName, float textSize) {
+			throw new System.NotImplementedException();
+		}
+
+		public SizeF GetStringSize(string value, string fontName, float textSize, HorizontalAlignment horizontalAlignment, VerticalAlignment verticalAlignment) {
+			throw new System.NotImplementedException();
+		}
+
+		public IImage LoadImageFromStream(Stream stream, ImageFormat format = ImageFormat.Png) {
+			throw new System.NotImplementedException();
+		}
+
+		public BitmapExportContext CreateBitmapExportContext(int width, int height, float displayScale = 1) {
+			throw new System.NotImplementedException();
+		}
+
+	}
+
+}
