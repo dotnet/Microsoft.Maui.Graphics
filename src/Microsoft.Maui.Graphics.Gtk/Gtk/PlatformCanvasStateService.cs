20dd6992f39fb9a6191dead666be15bfeddf60a0
lytico <lytico@users.sourceforge.net>
2022-07-01 15:06:40 +0200

    PlatformCanvasStateService.cs: modernize code

--- src/Microsoft.Maui.Graphics.Gtk/Gtk/PlatformCanvasStateService.cs
+++ src/Microsoft.Maui.Graphics.Gtk/Gtk/PlatformCanvasStateService.cs
@@ -1,12 +1,9 @@
 ﻿using System;
 
 namespace Microsoft.Maui.Graphics.Platform.Gtk {
-
 	public class PlatformCanvasStateService : ICanvasStateService<PlatformCanvasState> {
-		public PlatformCanvasState CreateNew (object context) =>
-			new PlatformCanvasState { };
+		public PlatformCanvasState CreateNew (object context) => new() { };
 
-		public PlatformCanvasState CreateCopy (PlatformCanvasState prototype) =>
-			new PlatformCanvasState (prototype);
+		public PlatformCanvasState CreateCopy (PlatformCanvasState prototype) => new(prototype);
 	}
 }
